58b87622f9c0e645e10151172cb87642d9201db1
Chen, Yishu <Yishu.Chen@firstdata.com>
2016-01-29 22:48:57 -0800

    Test Player Movement for a Client

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
