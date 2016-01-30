d63ff03dbff1a2f675fe63bd6eaaa5496b40592a
Chen, Yishu <Yishu.Chen@firstdata.com>
2016-01-30 00:06:40 -0800

    Test Multiplayer Movement

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+public class PlayerMove : NetworkBehaviour
+{
+	void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+
 		var x = Input.GetAxis("Horizontal")*0.1f;
 		var z = Input.GetAxis("Vertical")*0.1f;
 
