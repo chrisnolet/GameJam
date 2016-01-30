074909268437976ced5ce19751efe7bcce2fb64b
Chris Nolet <chris@relaunch.io>
2016-01-29 22:04:22 -0800

    Add canvas and game controller

---
+++ Assets/Scripts/Gamepad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Gamepad : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (OVRInput.GetDown(OVRInput.Button.SecondaryShoulder)) {
+
+    }
+  }
+}
