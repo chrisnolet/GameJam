05bf8411d2b5f3f797168f6e951fb842064c4809
Joe Darrin <joe.b.darrin@gmail.com>
2016-01-30 14:31:38 -0600

    Add moving platforms

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	[SerializeField]
+	Transform platform;
+
+	[SerializeField]
+	Transform startTransform;
+
+	[SerializeField]
+	Transform endTransform;
+
+	[SerializeField]
+	float platformSpeed;
+
+	Vector3 direction;
+	Transform destination;
+		
+	void Start (){
+		SetDestination(startTransform);
+	}
+
+	void FixedUpdate(){
+		platform.GetComponent<Rigidbody>().MovePosition(platform.position + direction * platformSpeed * Time.fixedDeltaTime);
+
+		if(Vector3.Distance (platform.position, destination.position) < platformSpeed * Time.fixedDeltaTime){
+			SetDestination(destination == startTransform ? endTransform : startTransform);
+		}
+	}
+
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(startTransform.position, platform.localScale);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(endTransform.position, platform.localScale);
+	}
+
+	void SetDestination(Transform dest){
+		destination = dest;
+		direction = (destination.position - platform.position).normalized;
+	}
+
+}
