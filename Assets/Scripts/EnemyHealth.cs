86b48b96e2b50bd0bbf00358a0c207441021acea
Goktug Yilmaz <goktugyil@gmail.com>
2016-01-30 14:23:30 -0800

    Created enemyhealthbar, at some point we need to move the healthbar to the enemy player object

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour {
+
+	//Constants
+	const float maxHealth = 100;
+
+	//Variables
+	private float curHealth;
+	public float currentHealth {
+		get { return curHealth; }
+		set {
+			curHealth = value;
+			updateBar ();
+		}
+	}
+	public Image healthBar;
+		
+	void Awake() {
+		currentHealth = maxHealth;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void updateBar() {
+		healthBar.transform.localScale = new Vector3 (currentHealth / maxHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+}
