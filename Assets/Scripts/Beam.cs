9cefa934aff0e0d9187c87de37998de166ccc320
Chris Nolet <chris@relaunch.io>
2016-01-31 13:59:52 -0800

    Reinstate beam position and rotation code, synced over the network

--- Assets/Scripts/Beam.cs
+++ Assets/Scripts/Beam.cs
@@ -6,22 +6,10 @@ public class Beam : NetworkBehaviour {
   [SyncVar]
   public float scale;
 
-  void OnStartServer() {
-    print("OnStartServer: " + scale);
-  }
-
-  void Awake() {
-    print("Awake: " + scale);
-  }
-
   // Use this for initialization
   void Start() {
-    print("Start: " + scale);
-
     var localScale = transform.localScale;
     localScale.y = scale;
-    localScale.x = scale;
-    localScale.z = scale;
     transform.localScale = localScale;
   }
 
